f7007f3ebbb9c498525b4e7a826cff109917a062
PRoy <sam469roger@gmail.com>
2023-12-06 04:32:31 +0530

    commented out unused route imports

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-const usersRouter = require('./routes/userRoutes');
-const postsRouter = require('./routes/postRoutes');
-const commentsRouter = require('./routes/commentRoutes');
-const categoriesRouter = require('./routes/categoryRoutes');
+// const usersRouter = require('./routes/userRoutes');
+// const postsRouter = require('./routes/postRoutes');
+// const commentsRouter = require('./routes/commentRoutes');
+// const categoriesRouter = require('./routes/categoryRoutes');
 
 const app = express();
 
@@ -16,13 +16,13 @@ app.use(express.json());
 
 //* routes
 // users subrouter
-app.use('/api/v1/users', usersRouter);
+// app.use('/api/v1/users', usersRouter);
 // posts subrouter
-app.use('/api/v1/posts', postsRouter);
+// app.use('/api/v1/posts', postsRouter);
 // comments subrouter
-app.use('/api/v1/comments', commentsRouter);
+// app.use('/api/v1/comments', commentsRouter);
 // categories subrouter
-app.use('/api/v1/categories', categoriesRouter);
+// app.use('/api/v1/categories', categoriesRouter);
 
 //TODO error handlers
 
